f0292a8bfabdb8bb6121e1a9822012a58fd043dc
Matrix <matrix0415@gmail.com>
2015-01-19 15:31:36 +0800

    assign score feature back, classifyMain bugs fixed.

--- processes.py
+++ processes.py
@@ -47,7 +47,7 @@ calSaveOriIdf ="%s"%opinionPerFile+"-%ddf"%maxDF+"-%s-original-idf.csv"
 calSaveRsCombine ="%s"%opinionPerFile+"-%ddf"%maxDF+"-%s-combine.csv"
 
 # Opinion Pick Setting
-opinionPickPerFile =10000                           #
+opinionPickPerFile =5000                           #
 
 # Assign Score Setting
 assignScoreLocation ="dataset/scores"
@@ -321,33 +321,40 @@ def assignScoreMain(opinionData, **kwargs):
 
 	scoreList =[]
 
-	polarityTag =np.array([polarity for polarity, i in opinionData])
-	opinions =[nltk.posTaggerFilter(str(i), acceptTagList =tagList) for polarity, i in opinionData]
-	opinions =[content for rsbool, content in opinions if rsbool]
+	if not isfile(pathjoin(assignScoreLocation, assignScoreScores%kwargs['name'])):
+		polarityTag =np.array([polarity for polarity, i in opinionData])
+		opinions =[nltk.posTaggerFilter(str(i), acceptTagList =tagList) for polarity, i in opinionData]
+		opinions =[content for rsbool, content in opinions if rsbool]
 
-	vec =vectorizer(ngram_range =(1,3), dtype ='float16')
-	rsarray =vec.fit_transform(opinions)
-	feature =vec.get_feature_names()
-	bywords =rsarray.toarray().T
-	print("Assigning Score-Process:\tassigning, %s"%kwargs['name'])
+		vec =vectorizer(ngram_range =(1,3), dtype ='float16')
+		rsarray =vec.fit_transform(opinions)
+		feature =vec.get_feature_names()
+		bywords =rsarray.toarray().T
+		print("Assigning Score-Process:\tassigning, %s"%kwargs['name'])
 
-	if 'lexicon' in kwargs:
-		senticnetObj =sentiscore(type=kwargs['lexicon'], rdfPath=kwargs['rdfPath'], tags=kwargs['tags'], nltkPath=nltkPath)
-		scoreList =[assignScoreFunction(token=w, sentimentScoreLibObj=senticnetObj, scoreType="polarity") for w in feature]
+		if 'lexicon' in kwargs:
+			senticnetObj =sentiscore(type=kwargs['lexicon'], rdfPath=kwargs['rdfPath'], tags=kwargs['tags'], nltkPath=nltkPath)
+			scoreList =[assignScoreFunction(token=w, sentimentScoreLibObj=senticnetObj, scoreType="polarity") for w in feature]
 
-	elif 'feature' and 'score' in kwargs:
-		scoreList =[assignScoreFunction(token=w, feature=kwargs['feature'], score=kwargs['score']) for w in feature]
+		elif 'feature' and 'score' in kwargs:
+			scoreList =[assignScoreFunction(token=w, feature=kwargs['feature'], score=kwargs['score']) for w in feature]
+
+		else:
+			print("Assigning Score-Process:\tError, none of the options.")
+
+		for index, s in enumerate(scoreList): bywords[index:index+1] *=s
+		resultArray =bywords.T
+
+		print("Assigning Score-Output:\tfinal")
+		fwrite(pathjoin(assignScoreLocation, assignScoreFeatures%kwargs['name']), "\n".join(feature))
+		np.savetxt(pathjoin(assignScoreLocation, assignScorePolarity%kwargs['name']), polarityTag, delimiter=",", fmt='%1.1i')
+		np.savetxt(pathjoin(assignScoreLocation, assignScoreScores%kwargs['name']), resultArray, delimiter=",", fmt='%1.7f')
 
 	else:
-		print("Assigning Score-Process:\tError, none of the options.")
-
-	for index, s in enumerate(scoreList): bywords[index:index+1] *=s
-	resultArray =bywords.T
-
-	print("Assigning Score-Output:\tfinal")
-	fwrite(pathjoin(assignScoreLocation, assignScoreFeatures%kwargs['name']), "\n".join(feature))
-	np.savetxt(pathjoin(assignScoreLocation, assignScorePolarity%kwargs['name']), polarityTag, delimiter=",", fmt='%1.1i')
-	np.savetxt(pathjoin(assignScoreLocation, assignScoreScores%kwargs['name']), resultArray, delimiter=",", fmt='%1.7f')
+		print("Assigning Score-Input:\tReading Files")
+		feature =fread(pathjoin(assignScoreLocation, assignScoreFeatures%kwargs['name']))[1].split("\n")
+		polarityTag =np.genfromtxt(pathjoin(assignScoreLocation, assignScorePolarity%kwargs['name']), delimiter=",")
+		resultArray =np.genfromtxt(pathjoin(assignScoreLocation, assignScoreScores%kwargs['name']), delimiter=",")
 
 	return (feature, resultArray, polarityTag)
 
@@ -357,14 +364,13 @@ def classifyMain(data):
 
 	rs =[]
 	model = svm.SVC()
-
-	for scores, tags in data:
-		accuracy =cv.cross_val_score(model, scores, tags, scoring='accuracy', n_jobs =classifyJobs, cv =cvFold)
-		avgPrecision =cv.cross_val_score(model, scores, tags, scoring='average_precision', n_jobs =classifyJobs, cv =cvFold)
-		f1 =cv.cross_val_score(model, scores, tags, scoring='f1', n_jobs =classifyJobs, cv =cvFold)
-		precision =cv.cross_val_score(model, scores, tags, scoring='precision', n_jobs =classifyJobs, cv =cvFold)
-		recall =cv.cross_val_score(model, scores, tags, scoring='recall', n_jobs =classifyJobs, cv =cvFold)
-		rs.append({"accuracy": accuracy, "avgPrecision": avgPrecision, "f1": f1, "precision": precision, "recall": recall})
+	scores, tags =data
+	accuracy =cv.cross_val_score(model, scores, tags, scoring='accuracy', n_jobs =classifyJobs, cv =cvFold)
+	avgPrecision =cv.cross_val_score(model, scores, tags, scoring='average_precision', n_jobs =classifyJobs, cv =cvFold)
+	f1 =cv.cross_val_score(model, scores, tags, scoring='f1', n_jobs =classifyJobs, cv =cvFold)
+	precision =cv.cross_val_score(model, scores, tags, scoring='precision', n_jobs =classifyJobs, cv =cvFold)
+	recall =cv.cross_val_score(model, scores, tags, scoring='recall', n_jobs =classifyJobs, cv =cvFold)
+	rs.append({"accuracy": accuracy, "avgPrecision": avgPrecision, "f1": f1, "precision": precision, "recall": recall})
 
 	fwrite(resultFile, str(rs))
 
